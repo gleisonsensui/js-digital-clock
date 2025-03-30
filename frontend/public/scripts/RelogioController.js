c08d17faf395ac8102496ac210e6c4877ac90a9f
gleisonsensui <gleison.nsilva@hotmail.com>
2025-03-30 14:50:18 -0300

    feat: created business rules to project

--- frontend/public/scripts/RelogioController.js
+++ frontend/public/scripts/RelogioController.js
@@ -1,8 +1,32 @@
-'use strict'
-import RelogioModel from "./RelogioModel.js"
+'use strict';
+import RelogioModel from './RelogioModel.js';
 
 export default class RelogioController {
-    constructor() {
-        this.relogio = new RelogioModel();
-    }
+  constructor() {
+    this.relogio = new RelogioModel();
+    this.entryHours = document.querySelector('.hours');
+    this.entryMinuts = document.querySelector('.minutes');
+    this.entrySeconds = document.querySelector('.seconds');
+
+    // Start digital clock
+    this.updateValueClock();
+  }
+
+
+  updateValueClock() {
+    this.updateClock();
+    setInterval(() => {
+      this.updateClock();
+    }, 1000);
+  }
+
+  updateClock() {
+    this.entryHours.textContent = this.getToday().getHours();
+    this.entryMinuts.textContent = this.getToday().getMinutes();
+    this.entrySeconds.textContent = this.getToday().getSeconds();
+  }
+
+  getToday() {
+    return new Date();
+  }
 }
--- frontend/public/scripts/RelogioModel.js
+++ frontend/public/scripts/RelogioModel.js
@@ -2,6 +2,32 @@
 
 export default class RelogioModel {
     constructor() {
-        
+        this._hours = 0;
+        this._minutes = 0;
+        this._seconds = 0;
+    }
+
+    get hours() {
+        return this._hours
+    }
+
+    set hours(value) {
+        this._hours = value;
+    }
+
+    get minutes() {
+        return this._minuts
+    }
+
+    set minutes(value) {
+        this._minuts = value;
+    }
+
+    get seconds() {
+        return this._seconds
+    }
+
+    set seconds(value) {
+        this._seconds = value;
     }
 }
