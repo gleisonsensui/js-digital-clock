f2e8ca2fef6dae0cd800f482133c457d59b3d5f1
gleisonsensui <gleison.nsilva@hotmail.com>
2025-03-30 14:30:58 -0300

    feat: created style format

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-// import '../public/styles/stylesheet.css'
+import '../public/styles/stylesheet.css'
 import RelogioController from "../public/scripts/RelogioController.js";
 
 const relogioDigital = new RelogioController();
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('node:path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,11 +10,16 @@ module.exports = {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: "development",
   module: {
     rules: [
         {
             test: /\.html$/,
             use: ['html-loader']
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
         }
     ]
   },
@@ -23,6 +29,9 @@ module.exports = {
         index: 'index.html',
         title: 'Relogio digital',        
         template: './public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
     })
   ],
   optimization: {
